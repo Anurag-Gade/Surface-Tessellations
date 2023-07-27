1f68f70e5cb975fabc96acc847832de59820a618
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2023-07-27 14:38:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from overlapping_tesselations import *
 
 input_file_path = "/input_dir/mri.nii"
 output_file_path = "/output_dir/overtess_1.stl"
+
+# type(offset) -> float
+    # Offset adjusts the overlapping distance.
+'''
+type(offset) -> float
+The variable "offset" adjusts the overlapping distance
+'''
 offset = 0.1
 iters = 100
 
