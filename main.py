46b3731fcc49a796a964c978509b0d92e65ee74d
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2024-01-08 12:43:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from overlapping_tesselations import *
+import argparse
 
-input_file_path = "/input_dir/mri.nii"
-output_file_path = "/output_dir/overtess_1.stl"
+parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter, description = "Surface Tessellations")
+
+parser.add_argument('--input_path', action= 'store', required=True,
+                    type=str, help='Path to an input nifti file')
+
+parser.add_argument('--output_file_path', action='store', required=True,
+                    type=str, help='Output file path')
+
+parser.add_argument('--offset', action='store', default=0.1,
+                    type=float, help='Overlapping offset')
+
+parser.add_argument('--iterations', action='store', default=100,
+                    type=int, help='Mesh decimation parameter')
+
+args = parser.parse_args() 
+
+input_path = args.input_path
+output_file_path = args.output_file_path
+offset = args.offset
+iterations = args.iterations
+
 
 # type(offset) -> float
     # Offset adjusts the overlapping distance.
@@ -9,7 +29,6 @@ output_file_path = "/output_dir/overtess_1.stl"
 type(offset) -> float
 The variable "offset" adjusts the overlapping distance
 '''
-offset = 0.1
-iters = 100
 
-tesselation_function(input_file_path, output_file_path, offset, iters) 
+
+tesselation_function(input_path, output_file_path, offset, iters) 
